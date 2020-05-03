49eb2129c90ee6827965032881f66e9ee164c752
mtgamemaker <64731657+mtgamemaker@users.noreply.github.com>
2020-05-03 14:01:11 +0300

    Add files via upload

---
+++ BackGroundMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundMove : MonoBehaviour
+{
+    public int speed;
+    public float startX;
+    public float endX;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+
+        if(transform.position.x < endX){
+            transform.position = new Vector2(startX,transform.position.y);
+
+        }
+    }
+}
