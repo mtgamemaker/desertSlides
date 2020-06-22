acf5d1944f94bc596ed1a6296ac4dbd709e06416
MT Game Maker <64731657+mtgamemaker@users.noreply.github.com>
2020-06-23 01:02:43 +0300

    Add files via upload

---
+++ TakePlayerDown.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakePlayerDown : MonoBehaviour
+{
+    private PlatformEffector2D pf;
+
+    float waitTime = 0.5f;
+
+    bool goingDown = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pf = GetComponent<PlatformEffector2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            pf.rotationalOffset = 180f;
+            goingDown = true;
+        }
+
+        if (goingDown)
+        {
+            waitTime -= Time.deltaTime;
+
+            if (waitTime <= 0)
+            {
+                pf.rotationalOffset = 0f;
+                goingDown = false;
+                waitTime = 0.5f;
+            }
+        }
+    }
+}
